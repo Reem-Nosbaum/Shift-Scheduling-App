56069c7016df3ab7498ad16f592badca66b5e925
Reem-Nosbaum <reemnu@gmail.com>
2023-12-30 12:04:07 +0200

    Improve structure / format of the code. :art:

---
+++ Shift-Scheduling-Frontend/src/pages/User.jsx
@@ -0,0 +1,31 @@
+import "../style/user.css";
+
+const shifts = [
+  {
+    Sunday: ["morning", "evening", "night"],
+  },
+  {
+    Monday: ["morning", "evening", "night"],
+  },
+  {
+    Tuesday: ["morning", "evening", "night"],
+  },
+  {
+    Wednesday: ["morning", "evening", "night"],
+  },
+  {
+    Thursday: ["morning", "evening", "night"],
+  },
+  {
+    Friday: ["morning", "evening", "night"],
+  },
+  {
+    Saturday: ["morning", "evening", "night"],
+  },
+];
+console.log(shifts[0]);
+function User() {
+  return <div></div>;
+}
+
+export default User;
